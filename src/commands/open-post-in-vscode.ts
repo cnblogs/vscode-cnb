b58f5f456696c120e9b274242d69461582232c8e
laggage <laggage-la@outlook.com>
2021-12-27 15:46:34 +0800

    feat: tweak filename of post downloaded from remote cnblogs server

--- src/commands/open-post-in-vscode.ts
+++ src/commands/open-post-in-vscode.ts
@@ -1,25 +1,31 @@
 import * as fs from 'fs';
-import { FileSystemError, Uri, workspace } from 'vscode';
+import path = require('path');
+import { FileSystemError, MessageOptions, Uri, window, workspace } from 'vscode';
+import { BlogPost } from '../models/blog-post';
 import { AlertService } from '../services/alert.service';
 import { blogPostService } from '../services/blog-post.service';
 import { PostFileMapManager } from '../services/post-file-map';
 import { Settings } from '../services/settings.service';
 import { openPostFile } from './open-post-file';
 
-export const openPostInVscode = async (postId: number, forceUpdatePostFile = false) => {
-    const mappedPostFilePath = PostFileMapManager.getFilePath(postId);
-    const isFileExist = await new Promise<boolean>(resolve => {
-        if (!mappedPostFilePath) {
-            resolve(false);
-            return;
-        }
-        fs.stat(mappedPostFilePath, (err, stat) => {
-            resolve(!err && stat.isFile());
-        });
-    });
-    if (isFileExist && !forceUpdatePostFile) {
+const generateLocalPostFileUri = (post: BlogPost, includePostId = false): Uri => {
+    const workspaceUri = Settings.workspaceUri;
+    const ext = `.${post.isMarkdown ? 'md' : 'html'}`;
+    const postIdSegment = includePostId ? `.${post.id}` : '';
+    return Uri.joinPath(workspaceUri, `${post.title}${postIdSegment}${ext}`);
+};
+
+export const openPostInVscode = async (postId: number, forceUpdateLocalPostFile = false) => {
+    let mappedPostFilePath = PostFileMapManager.getFilePath(postId);
+    const isFileExist = mappedPostFilePath ? fs.existsSync(mappedPostFilePath) : false;
+    if (isFileExist && !forceUpdateLocalPostFile) {
         await openPostFile(mappedPostFilePath!);
         return;
+    }
+    // 本地文件已经被删除了, 确保重新生成博文与本地文件的关联
+    if (mappedPostFilePath && !isFileExist) {
+        await PostFileMapManager.updateOrCreate(postId, '');
+        mappedPostFilePath = undefined;
     }
 
     const postEditDto = await blogPostService.fetchPostEditDto(postId);
@@ -27,9 +33,33 @@ export const openPostInVscode = async (postId: number, forceUpdatePostFile = fal
 
     const workspaceUri = Settings.workspaceUri;
     await createDirectoryIfNotExist(workspaceUri);
-    const fileUri = mappedPostFilePath
-        ? Uri.file(mappedPostFilePath!)
-        : Uri.joinPath(workspaceUri, `${postId}-${post.title}.${postEditDto.post.isMarkdown ? 'md' : 'html'}`);
+    let fileUri = mappedPostFilePath ? Uri.file(mappedPostFilePath) : generateLocalPostFileUri(post);
+
+    // 博文尚未关联到本地文件的情况
+    if (!mappedPostFilePath) {
+        // 本地存在和博文同名的文件, 询问用户是要覆盖还是同时保留两者
+        if (fs.existsSync(fileUri.fsPath)) {
+            let conflictOptions = [
+                '保留本地文件(这会新建另一个文件名中包含博文id的文件)',
+                '覆盖本地文件(会导致本地文件中内容丢失)',
+            ];
+            const selectedOption = await window.showInformationMessage(
+                `无法保存博文到本地以进行编辑, 文件名冲突`,
+                { detail: `本地已存在名为"${path.basename(fileUri.fsPath)}"的文件`, modal: true } as MessageOptions,
+                ...conflictOptions
+            );
+            switch (selectedOption) {
+                case conflictOptions[0]:
+                    fileUri = generateLocalPostFileUri(post, true);
+                    break;
+                // 取消, 直接返回, 不进行任何操作
+                case undefined:
+                    return;
+            }
+        }
+    }
+
+    // 博文内容写入本地文件, 若文件不存在, 会自动创建对应的文件
     await workspace.fs.writeFile(fileUri, new TextEncoder().encode(postEditDto.post.postBody));
     await PostFileMapManager.updateOrCreate(postId, fileUri.fsPath);
     await openPostFile(post);
