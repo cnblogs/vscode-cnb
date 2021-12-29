ceafc60a0631d059c14f8b9c1a66d4d0fca03e6c
laggage <laggage-la@outlook.com>
2021-12-29 10:36:44 +0800

    fix: fix edit post in i.cnblogs.com

--- src/commands/open-post-in-blog-admin.ts
+++ src/commands/open-post-in-blog-admin.ts
@@ -1,12 +1,13 @@
 import { commands, Uri } from 'vscode';
 import { BlogPost } from '../models/blog-post';
+import { PostFileMapManager } from '../services/post-file-map';
 
 export const openPostInBlogAdmin = (item: BlogPost | Uri) => {
     if (!item) {
         return;
     }
 
-    const postId = item instanceof BlogPost ? item.id : -1;
+    const postId = item instanceof BlogPost ? item.id : PostFileMapManager.getPostId(item.fsPath) ?? -1;
 
     commands.executeCommand('vscode.open', Uri.parse(`https://i.cnblogs.com/posts/edit;postId=${postId}`));
 };
