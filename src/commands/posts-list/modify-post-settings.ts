65875c512c9b47d893aaf376fc587ddd5d653b0e
laggage <laggage-la@outlook.com>
2021-12-29 16:03:38 +0800

    feat: add modify post settings command to context menu of  explorer item

--- src/commands/posts-list/modify-post-settings.ts
+++ src/commands/posts-list/modify-post-settings.ts
@@ -1,18 +1,35 @@
+import { Uri } from 'vscode';
 import { BlogPost } from '../../models/blog-post';
 import { AlertService } from '../../services/alert.service';
 import { blogPostService } from '../../services/blog-post.service';
+import { PostFileMapManager } from '../../services/post-file-map';
 import { extensionViews } from '../../tree-view-providers/tree-view-registration';
 import { inputPostSettings } from '../../utils/input-post-settings';
 
-export const modifyPostSettings = async (post: BlogPost) => {
-    if (!post || post.id < 0) {
+export const modifyPostSettings = async (input: BlogPost | Uri) => {
+    let post: BlogPost | undefined;
+    let postId = -1;
+    if (input instanceof BlogPost) {
+        post = input;
+        postId = input.id;
+    } else if (input instanceof Uri) {
+        postId = PostFileMapManager.getPostId(input.fsPath) ?? -1;
+        if (postId < 0) {
+            AlertService.warning('本地文件尚未关联到博文');
+            return;
+        }
+    }
+
+    if (!(postId >= 0)) {
         return;
     }
 
-    await extensionViews.postsList?.reveal(post);
-    const editDto = await blogPostService.fetchPostEditDto(post.id);
+    if (post) {
+        await extensionViews.postsList?.reveal(post);
+    }
+    const editDto = await blogPostService.fetchPostEditDto(postId);
     const postEditDto = editDto.post;
-    const inputSettings = await inputPostSettings(post.title, postEditDto);
+    const inputSettings = await inputPostSettings(postEditDto.title, postEditDto);
     if (!inputSettings) {
         return;
     }
