127da44ee94f4411316a588f7817c1d8a32795ab
laggage <38829279+laggage@users.noreply.github.com>
2022-04-01 19:16:20 +0800

    feat: add uri handler used to open a existing post (#46)

---
+++ src/services/edit-post-uri-handler.ts
@@ -0,0 +1,15 @@
+import { ProviderResult, Uri, UriHandler } from 'vscode';
+import { openPostInVscode } from '../commands/posts-list/open-post-in-vscode';
+
+export class EditPostUriHandler implements UriHandler {
+    handleUri(uri: Uri): ProviderResult<void> {
+        const { path } = uri;
+        const splits = path.split('/');
+        if (splits.length >= 3 && splits[1] === 'edit-post') {
+            const postId = parseInt(splits[2]);
+            if (postId > 0) {
+                openPostInVscode(postId).then(undefined, () => void 0);
+            }
+        }
+    }
+}
