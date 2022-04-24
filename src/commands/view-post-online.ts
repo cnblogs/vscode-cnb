8c98be9a1637d64ec7e7fb806e73e85bf7bf603c
laggage <38829279+laggage@users.noreply.github.com>
2022-04-24 14:56:06 +0800

    ui: make view-post-online command visible in vscode command palette (#52)

--- src/commands/view-post-online.ts
+++ src/commands/view-post-online.ts
@@ -1,10 +1,13 @@
-import { commands, Uri } from 'vscode';
+import { commands, Uri, window } from 'vscode';
 import { Post } from '../models/post';
 import { postService } from '../services/post.service';
 import { PostFileMapManager } from '../services/post-file-map';
 
-export const viewPostOnline = async (input: Post | Uri) => {
+export const viewPostOnline = async (input?: Post | Uri) => {
     let post: Post | undefined = input instanceof Post ? input : undefined;
+    if (!input) {
+        input = window.activeTextEditor?.document.uri;
+    }
     if (input instanceof Uri) {
         const postId = PostFileMapManager.getPostId(input.fsPath);
         if (postId) {
