be836f9e63fbd14424120899b51ae13117d8d618
Thaumy <thaumy@outlook.com>
2023-07-30 14:29:05 +0800

    feat: http post

--- rs/src/http.rs
+++ rs/src/http.rs
@@ -1,1 +1,23 @@
 pub mod get;
+pub mod post;
+
+use alloc::string::String;
+use core::convert::TryFrom;
+use core::str::FromStr;
+use reqwest::header::HeaderMap;
+use serde_json::Value;
+use wasm_bindgen::prelude::*;
+use anyhow::Result;
+use wasm_bindgen::__rt::std::collections::HashMap;
+use crate::infra::result::IntoResult;
+
+#[wasm_bindgen(js_name = RsHttp)]
+pub struct RsHttp;
+
+fn header_json_to_header_map(header_json: &str) -> Result<HeaderMap> {
+    let header_json = Value::from_str(header_json)?;
+    let header = serde_json::from_value::<HashMap<String, String>>(header_json)?;
+    let header_map = HeaderMap::try_from(&header)?;
+
+    header_map.into_ok()
+}
--- rs/src/http/post.rs
+++ rs/src/http/post.rs
@@ -6,21 +6,21 @@ use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen(js_class = RsHttp)]
 impl RsHttp {
-    #[wasm_bindgen(js_name = get)]
-    pub async fn export_get(url: &str, header_json: &str) -> Result<String, String> {
-        let body = get(url, header_json).await;
+    #[wasm_bindgen(js_name = post)]
+    pub async fn export_post(url: &str, body: String, header_json: &str) -> Result<String, String> {
+        let body = post(url, body, header_json).await;
         let Ok(body) = body else { return body.unwrap_err().to_string().into_err(); };
 
         body.into_ok()
     }
 }
 
-async fn get(url: &str, header_json: &str) -> Result<String> {
+async fn post(url: &str, body: String, header_json: &str) -> Result<String> {
     let header_map = header_json_to_header_map(header_json)?;
 
     let client = reqwest::Client::new();
 
-    let resp = client.get(url).headers(header_map).send().await?;
+    let resp = client.post(url).headers(header_map).body(body).send().await?;
 
     let body = resp.text().await?;
 
