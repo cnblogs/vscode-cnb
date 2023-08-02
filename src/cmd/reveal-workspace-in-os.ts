95cf46d1577421f0579195a7027bdf9c96b78e9f
Thaumy <thaumy@outlook.com>
2023-08-02 15:30:22 +0800

    Revert "chore: resolve merge conflicts"
    This reverts commit 107b5bb9c8aaa26d926108a17ce930eb88ba7c31, reversing
    changes made to 5b48e9871f7ea7d0f9b3eee7ee36a7fb9dda80de.

--- src/cmd/reveal-workspace-in-os.ts
+++
@@ -1,4 +0,0 @@
-import { execCmd } from '@/infra/cmd'
-import { WorkspaceCfg } from '@/ctx/cfg/workspace'
-
-export const revealWorkspaceInOs = () => execCmd('revealFileInOS', WorkspaceCfg.getWorkspaceUri())
--- src/infra/rm-yfm.ts
+++
@@ -1,5 +0,0 @@
-// remove YAML front matter in markdown
-export function rmYfm(mkd: string) {
-    const reg = /^---\n(\n|.)*?\n---\n*/g
-    return mkd.replace(reg, '')
-}
