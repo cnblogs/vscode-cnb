1094fc355bd91b5ba10be4be6e3f0217821de460
Thaumy <thaumy@outlook.com>
2023-08-16 13:18:44 +0800

    refactor: use url macro

--- rs/src/cnb/user/get_info.rs
+++ rs/src/cnb/user/get_info.rs
@@ -2,7 +2,7 @@ use crate::cnb::user::{UserReq, API_BASE_URL};
 use crate::http::body_or_err;
 use crate::infra::http::setup_auth;
 use crate::infra::result::{HomoResult, ResultExt};
-use crate::panic_hook;
+use crate::{openapi, panic_hook};
 use alloc::format;
 use alloc::string::String;
 use anyhow::Result;
@@ -13,7 +13,7 @@ impl UserReq {
     #[wasm_bindgen(js_name = getInfo)]
     pub async fn export_get_info(&self) -> HomoResult<String> {
         panic_hook!();
-        let url = format!("{}/users", API_BASE_URL);
+        let url = openapi!("/users");
 
         let client = reqwest::Client::new().get(url);
 
