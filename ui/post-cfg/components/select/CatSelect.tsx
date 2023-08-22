63604dc2ae67acd621f4b3cc958583a38d27ee4f
Thaumy <thaumy@outlook.com>
2023-08-22 15:22:53 +0800

    refactor: CatSelect

--- ui/post-cfg/components/select/CatSelect.tsx
+++ ui/post-cfg/components/select/CatSelect.tsx
@@ -1,57 +1,48 @@
-import { Checkbox, Stack } from '@fluentui/react'
+import { ComboBox } from '@fluentui/react'
 import { Component } from 'react'
 import { PostCategory } from '@/model/post-category'
-import { eq } from '../../../../src/infra/fp/ord'
-import { PersonalCategoryStore } from '../../service/personal-category-store'
 
 type Props = {
-    categoryIds: number[] | undefined
+    allCats: PostCategory[]
+    selectedCatIds: number[]
     onChange: (categoryIds: number[]) => void
 }
+type State = { selectedCatIds: number[] }
 
-type State = {
-    categories: PostCategory[]
-    categoryIds: number[]
-}
-
-class CatSelect extends Component<Props, State> {
+export class CatSelect extends Component<Props, State> {
     constructor(props: Props) {
         super(props)
-        this.state = {
-            categories: PersonalCategoryStore.get(),
-            categoryIds: props.categoryIds ?? [],
-        }
+        this.state = { selectedCatIds: this.props.selectedCatIds }
     }
 
     render() {
-        const categories = this.state.categories
-        const categoryIds = this.state.categoryIds
-        const items = categories.map(category => (
-            <Checkbox
-                key={category.categoryId}
-                onChange={(_, isChecked) => this.onCheckboxChanged(category.categoryId, isChecked)}
-                label={category.title}
-                checked={categoryIds?.includes(category.categoryId)}
-            />
-        ))
+        const opts = this.props.allCats.map(cat => ({
+            data: cat.categoryId,
+            key: cat.categoryId,
+            text: cat.title,
+        }))
+
         return (
-            <Stack tokens={{ childrenGap: 16 }} horizontal wrap>
-                {items}
-            </Stack>
+            <ComboBox
+                label="分类"
+                placeholder="点击选择"
+                options={opts}
+                selectedKey={this.state.selectedCatIds}
+                multiSelect
+                useComboBoxAsMenuWidth
+                onChange={(_, opt, __, val) => {
+                    if (opt !== undefined) {
+                        if (val === undefined) {
+                            const selectedCatIds = this.state.selectedCatIds.filter(x => x !== opt.data)
+                            this.setState({ selectedCatIds })
+                        } else {
+                            this.state.selectedCatIds.push(opt.data as number)
+                            this.setState(this.state)
+                        }
+                    }
+                    this.props.onChange(this.state.selectedCatIds)
+                }}
+            />
         )
     }
-
-    private onCheckboxChanged(categoryId: number, isChecked = false) {
-        const { categoryIds } = this.state
-
-        const position = categoryIds.findIndex(eq(categoryId))
-        const isInclude = position >= 0
-
-        if (isChecked && !isInclude) categoryIds.push(categoryId)
-        else if (isInclude) categoryIds.splice(position, 1)
-
-        this.props.onChange?.apply(this, [categoryIds])
-    }
 }
-
-export { CatSelect }
