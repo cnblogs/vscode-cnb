f3faf017329161d179f3812b46bd1f12714f8616
dudu <dudu@cnblogs.com>
2023-09-15 19:55:38 +0800

    debug: print path

--- src/service/parse-webview-html.ts
+++ src/service/parse-webview-html.ts
@@ -5,6 +5,9 @@ export type WebviewEntryName = 'ing' | 'post-cfg'
 
 export async function parseWebviewHtml(entry: WebviewEntryName, webview: vscode.Webview) {
     const path = vscode.Uri.joinPath(globalCtx.assetsUri, 'ui', entry, 'index.html')
+    const channel = vscode.window.createOutputChannel('vscode-cnb')
+    channel.appendLine('path: ' + path.fsPath)
     const file = await vscode.workspace.fs.readFile(path)
+    channel.appendLine('vscode.workspace.fs.readFile')
     return file.toString().replace(/@PWD/g, webview.asWebviewUri(globalCtx.assetsUri).toString())
 }
