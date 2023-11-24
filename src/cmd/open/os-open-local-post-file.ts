4fb97e57d35be3bf1302ac0cdd24a4abb07225fc
dudu <dudu@cnblogs.com>
2023-11-24 13:13:29 +0800

    debug: add console.log

--- src/cmd/open/os-open-local-post-file.ts
+++ src/cmd/open/os-open-local-post-file.ts
@@ -4,10 +4,14 @@ import { Post } from '@/model/post'
 import { PostFileMapManager } from '@/service/post/post-file-map'
 
 export function osOpenLocalPostFile(post?: Post) {
+    console.log('post === undefined ? ' + (post === undefined))
     if (post === undefined) return
 
+    console.log('post.id: ' + post.id)
     const postFilePath = PostFileMapManager.getFilePath(post.id)
     if (postFilePath === undefined) return
+
+    console.log('postFilePath: ' + postFilePath)
 
     return execCmd('revealFileInOS', Uri.file(postFilePath))
 }
