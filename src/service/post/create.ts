d876cb6cadc29fd0a396d888bd8ef6d8c6ec6a87
dudu <dudu@cnblogs.com>
2023-12-21 22:00:34 +0800

    refactor: creating post

--- src/service/post/create.ts
+++ src/service/post/create.ts
@@ -1,12 +1,11 @@
 import { homedir } from 'os'
-import path from 'path'
-import { Uri, window, workspace } from 'vscode'
-import { osOpenActiveFile } from '@/cmd/open/os-open-active-file'
+import { window } from 'vscode'
 import { WorkspaceCfg } from '@/ctx/cfg/workspace'
-import { saveLocalPost } from '@/cmd/post-list/upload-post'
 import { openPostFile } from '@/cmd/post-list/open-post-file'
-import { LocalPost } from '@/service/local-post'
-import { fsUtil } from '@/infra/fs/fsUtil'
+import { Post, PostType } from '@/model/post'
+import { PostService } from './post'
+import { Alert } from '@/infra/alert'
+import { PostListView } from '@/cmd/post-list/post-list-view'
 
 export async function createPost() {
     const workspacePath = WorkspaceCfg.getWorkspaceUri().fsPath
@@ -23,29 +22,18 @@ export async function createPost() {
 
     if (title == null) return
 
-    const filePath = path.join(workspacePath, `${title}.md`)
-
-    if (!(await fsUtil.exists(filePath)))
-        await workspace.fs.writeFile(Uri.file(filePath), Buffer.from('# Hello World\n'))
-
-    await openPostFile(filePath)
-    await osOpenActiveFile()
-    // 设置中关闭了 `autoReveal` 的情况下, 需要两次调用 `workbench.files.action.showActiveFileInExplorer` 命令, 才能正确 `reveal`
-    if (workspace.getConfiguration('explorer').get<boolean>('autoReveal') === false) await osOpenActiveFile()
-
-    const focusEditor = async () => {
-        const editor = window.activeTextEditor
-        if (editor !== undefined)
-            await window.showTextDocument(editor.document, { preview: false, preserveFocus: false })
+    const post = new Post()
+    post.title = title
+    post.postBody = '# Hello World\n'
+    post.isMarkdown = true
+    post.isDraft = true
+    post.postType = PostType.blogPost
+    const postId = (await PostService.update(post)).id
+    if (postId > 0) {
+        post.id = postId
+        await PostListView.refresh()
+        await openPostFile(post, undefined, true)
+    } else {
+        void Alert.err('创建博文失败，postId: ' + postId)
     }
-    await focusEditor()
-    // 确保能 focus 到编辑器(不这么做, 有时会聚焦到 explorer 处)
-    await new Promise<void>(resolve => {
-        const innerTimeout = setTimeout(() => {
-            clearTimeout(innerTimeout)
-            void focusEditor().finally(() => resolve())
-        }, 50)
-    })
-
-    await saveLocalPost(new LocalPost(filePath))
 }
