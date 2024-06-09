63866421aa203d9d0e17fca6c57733a00da2c9e8
dudu <dudu@cnblogs.com>
2024-06-09 18:06:58 +0800

    fix: remove unused code to work with vscode 1.90

--- src/service/code-challenge.ts
+++ src/service/code-challenge.ts
@@ -2,13 +2,6 @@ import base64url from 'base64url'
 import crypto from 'crypto'
 import { RsRand } from '@/wasm'
 
-// See in: https://docs.rs/getrandom/latest/getrandom/#nodejs-es-module-support
-/* eslint-disable */
-import { webcrypto } from 'node:crypto'
-// @ts-ignore
-globalThis.crypto = webcrypto
-/* eslint-disable */
-
 export const genVerifyChallengePair = () => {
     const verifyCode = RsRand.string(128)
     const base64Digest = crypto.createHash('sha256').update(verifyCode).digest('base64')
